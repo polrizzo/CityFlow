57a56f871da075b2b8c46643aef1d91301fddef6
polrizzo <paolo.rizzo.ds@gmail.com>
2024-12-20 15:02:10 +0100

    new baseline

--- trial_and_error/gen_simulation.sh
+++ trial_and_error/gen_simulation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Call the Python script with arguments
-python3 ../tools/generator/generate_grid_scenario.py 1 3 --numLeftLanes 1 --numStraightLanes 1 --numRightLanes 1 --turn
+python3 ../tools/generator/generate_grid_scenario.py 1 1 --numLeftLanes 0 --numStraightLanes 1 --numRightLanes 0 --turn
